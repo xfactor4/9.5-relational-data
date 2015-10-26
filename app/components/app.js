1bbcc5015eab0ac1c835d8d72453b691be5e5461
Jacob Smith <jacob@jacobsmith.io>
2015-10-26 12:02:07 -0400

    Implement logout

--- app/components/app.js
+++ app/components/app.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
+import store from '../store';
 
 var App = React.createClass({
   propTypes: {
     children: React.PropTypes.node
   },
 
+  handleLogout(e) {
+    e.preventDefault();
+    session.invalidate();
+  },
+
   render() {
+    let session = store.getSession();
+    let loggedIn = session.isAuthenticated();
+    let currentUser = session.get('currentUser');
+    let username = (currentUser && currentUser.get('username')) || 'Me';
+
     return (
       <div>
         <nav className="top-bar" data-topbar role="navigation">
@@ -20,6 +31,17 @@ var App = React.createClass({
             {/* Left Nav Section */}
             <ul className="left">
               <li><Link to="/create">Create</Link></li>
+            </ul>
+
+            <ul className="right">
+              {loggedIn &&
+              <li className="has-dropdown">
+                <a href="#">{username}</a>
+                <ul className="dropdown">
+                  <li><a href="#" onClick={this.handleLogout}>Logout</a></li>
+                </ul>
+              </li>
+              }
             </ul>
           </section>
         </nav>
