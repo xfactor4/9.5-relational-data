3fc5813dc635b65815aab052595542e2d8c65acf
Jacob Smith <jacob@jacobsmith.io>
2015-10-26 16:33:00 -0400

    Put the UD in CRUD

--- app/components/show-recipe.js
+++ app/components/show-recipe.js
@@ -2,10 +2,15 @@ import React from 'react';
 import store from '../store';
 import Recipe from '../models/recipe';
 import CommentsCollection from '../models/comments-collection';
+import { History } from 'react-router';
 
 const ShowRecipe = React.createClass({
+  mixins: [History],
+
   getInitialState() {
-    return {};
+    return {
+      isEditing: false
+    };
   },
 
   componentWillMount() {
@@ -18,17 +23,57 @@ const ShowRecipe = React.createClass({
     comments.fetch().then(() => this.setState({comments: comments}));
   },
 
+  handleEdit() {
+    this.setState({
+      isEditing: !this.state.isEditing
+    });
+  },
+
+  handleSave(e) {
+    e.preventDefault();
+    this.state.recipe.set({
+      name: this.refs.name.value
+    });
+
+    this.state.recipe.save();
+
+    this.setState({
+      isEditing: false
+    });
+  },
+
+  handleDestroy(e) {
+    e.preventDefault();
+    if(confirm("Are you sure?")){
+      this.state.recipe.destroy();
+      this.history.replaceState(null, '/');
+    }
+  },
+
   render() {
     let recipe = (this.state.recipe && this.state.recipe.toJSON()) || {};
     let comments = (this.state.comments && this.state.comments.toJSON()) || [];
-    return (
-      <div>
-        <h1>{recipe.name}</h1>
-        <ul>
-          {comments.map((c) => <li key={c.objectId}>{c.text}</li> )}
-        </ul>
-      </div>
-    )
+    let content;
+    if(this.state.isEditing) {
+      content = (
+        <form>
+          <input type="text" defaultValue={recipe.name} ref="name" />
+          <button type="submit" onClick={this.handleSave}>Save</button>
+        </form>
+      );
+    } else {
+      content = (
+        <div>
+          <h1>{recipe.name}</h1>
+          <button onClick={this.handleEdit}>Edit</button>
+          <button className="alert" onClick={this.handleDestroy}>Destroy</button>
+          <ul>
+            {comments.map((c) => <li key={c.objectId}>{c.text}</li> )}
+          </ul>
+        </div>
+      );
+    }
+    return content;
   }
 });
 
